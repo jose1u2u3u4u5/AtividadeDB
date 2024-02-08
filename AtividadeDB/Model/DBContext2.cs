091ace5af38e7fcd28a7069e60d2b3686dc4d56d
Oruam <96359831+jose1u2u3u4u5@users.noreply.github.com>
2024-02-07 22:21:15 -0300

    mariadbupdate

---
+++ AtividadeDB/Model/DBContext2.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AtividadeDB.Model
+{
+    public class DBContext2 : DbContext
+    {
+        public DbSet<Pessoas> Pessoas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
+        {
+            IConfigurationRoot Configuration = new ConfigurationBuilder()
+               .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+               .AddJsonFile("appsettings.json")
+            .Build();
+
+            var ConnectionString = Configuration.GetConnectionString("MariaDB");
+            OptionsBuilder.UseMySql(ConnectionString, ServerVersion.AutoDetect(ConnectionString));
+        }
+    }
+}
---
+++ AtividadeDB/Model/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtividadeDB.Model
+{
+    public class Pessoas
+    {
+        public int ID { get; set; }
+
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [MaxLength(15)]
+        public string? NumeroCelular { get; set; }
+
+        public DateOnly DataNascimento { get; set; }
+
+
+    }
+}
